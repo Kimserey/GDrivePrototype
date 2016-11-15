e8ed5130dd24b06ee32aaf491386e13f784576a7
Kimserey Lam <kimserey.lam@gmail.com>
2016-11-15 22:14:00 +0000

    initial commit

---
+++ GDrivePrototype/GDrivePrototype.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace GDrivePrototype
+{
+	public class App : Application
+	{
+		public App()
+		{
+			// The root page of your application
+			var content = new ContentPage
+			{
+				Title = "GDrivePrototype",
+				Content = new StackLayout
+				{
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Label {
+							Text = "Google Drive integration prototype"
+						}
+					}
+				}
+			};
+
+			MainPage = new NavigationPage(content);
+		}
+	}
+}
