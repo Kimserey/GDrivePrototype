c53e5635244ae3672b2f47af6db6fac921e4394a
Kimserey Lam <kimserey.lam@gmail.com>
2016-11-16 08:59:21 +0000

    comments

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -34,6 +34,13 @@ namespace GDrivePrototype.Droid
 		{
 			base.OnCreate(savedInstanceState);
 
+
+			// Remember to:
+			// 1. Enable Google Drive on the Google API developer console.
+			// 2. Create credentials using the package name and SHA-1 signature
+			//         - For debug, the SHA-1 must be taken from the debug keystore used by Xamarin to sign the app
+			//         - For release, the SHA-1 must be taken from the keystore used to sign the app
+			//
 			apiClient =
 				new GoogleApiClient
 					.Builder(this)
